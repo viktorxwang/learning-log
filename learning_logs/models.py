d9e7e8c57e58bce28a1c8ae221d147fdd260b1fb
Viktor Wang <viktorxwang@gmail.com>
2026-05-29 01:23:03 -0400

    Create model Topic

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# A model tells Django how to work with the stored data within the app.
+
+class Topic(models.Model):
+    """A topic the user is learning about. Encompasses other objects within the Topic header."""
+    text = models.CharField(max_length = 200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation/description of the model."""
+        return self.text
