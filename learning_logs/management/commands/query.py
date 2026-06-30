7c59d276c60e13767aba523b185ed15c7a8514f5
Viktor Wang <viktorxwang@gmail.com>
2026-06-30 00:38:38 -0400

    some formatting stuff

--- learning_logs/management/commands/query.py
+++ learning_logs/management/commands/query.py
@@ -9,9 +9,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Getting topics...")
         topics = Topic.objects.all()
+
         if len(topics) == 0:
             self.stdout.write(self.style.ERROR("Found no topics."))
         else:
             self.stdout.write(self.style.SUCCESS(f"There are {len(topics)} topics."))
+            
         for topic in topics:
             self.stdout.write(f"{topic.id} {topic}")
