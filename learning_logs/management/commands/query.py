3255be866b7c987f3207d8e89f4684e93c43a618
Viktor Wang <viktorxwang@gmail.com>
2026-06-30 00:37:56 -0400

    style query

--- learning_logs/management/commands/query.py
+++ learning_logs/management/commands/query.py
@@ -7,6 +7,11 @@ class Command(BaseCommand):
     help = "Prints all topics"
 
     def handle(self, *args, **options):
+        print("Getting topics...")
         topics = Topic.objects.all()
+        if len(topics) == 0:
+            self.stdout.write(self.style.ERROR("Found no topics."))
+        else:
+            self.stdout.write(self.style.SUCCESS(f"There are {len(topics)} topics."))
         for topic in topics:
             self.stdout.write(f"{topic.id} {topic}")
