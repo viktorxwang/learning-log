beb26eb0087679920b9f8bf056adc89c1e8b44cc
Viktor Wang <viktorxwang@gmail.com>
2026-08-02 18:23:36 -0400

    Added topics page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path("", views.index, name = "index"),
+    # Page that shows all the topics. Uses a loop to list the topics.
+    path("topics/", views.topics, name = "topics"),
 ]
