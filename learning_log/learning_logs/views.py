a405dbfdcb0d10b17293cdeda2bd10aa108a207b
Viktor Wang <viktorxwang@gmail.com>
2026-08-16 23:36:11 -0400

    Restrict access to Topics page

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry # We don't need to import Entry since it is implicit
 from .forms import TopicForm, EntryForm
@@ -9,6 +10,7 @@ def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     """Shows all topics."""
     topics = Topic.objects.order_by("date_added")
