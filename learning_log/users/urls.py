85fc9db5adb5ed3cd0af707080ea776279197452
Viktor Wang <viktorxwang@gmail.com>
2026-08-16 22:32:09 -0400

    Update urls.py

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -2,8 +2,13 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = "users"
 urlpatterns = [
     # Include default auth urls.
     path('', include('django.contrib.auth.urls')), # Sends the user to http://localhost:8000/users/login
+    # Registration page.
+    path('register/', views.register, name="register")
+
 ]
