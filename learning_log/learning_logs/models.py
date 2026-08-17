7976ea737e219ad26c724a629f3f7ba67c65b253
Viktor Wang <viktorxwang@gmail.com>
2026-08-17 00:18:41 -0400

    User association in Topic model

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # A model tells Django how to work with the stored data within the app.
+
+"""
+We need to connect the data to the user who submitted it. 
+We need to connect only the data highest in the hierarchy to a user, and the lower-level
+data will follow. For example, topics are the highest level of data.
+"""
 
 class Topic(models.Model):
     """A topic the user is learning about. Encompasses other objects within the Topic header."""
     text = models.CharField(max_length = 200)
     date_added = models.DateTimeField(auto_now_add=True)
+    # If a user is deleted, all the topics associated with that user will be deleted as well.
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation/description of the model."""
